44f3f7dfaefd292d50bf160db0afe15ca842e00e
B23-User <ikramazim@hotmail.com>
2022-08-03 12:35:50 -0400

    initial commit

--- src/main/java/GitPractice.java
+++ src/main/java/GitPractice.java
@@ -4,5 +4,9 @@ public class GitPractice {
     public static void main(String[] args) {
         System.out.println("hello");
         //test
+<<<<<<< HEAD
+=======
+        //test1
+>>>>>>> 8a19a25 (initial commit)
     }
 }
